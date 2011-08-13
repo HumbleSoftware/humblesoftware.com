cc1721196a9805b515b1574c3dd26dd6fe6e9011
Carl Sutherland <carl@humblesoftware.com>
2011-08-13 09:21:30 -0400

    Added initial projects page.

---
+++ application/views/projects.php
@@ -0,0 +1,64 @@
+<h2 class="project-title"><a href="<?php echo base_url(); ?>finance/index">HumbleFinance</a></h2>
+
+<div class="project-summary">
+
+	<span class="icon">
+		<a href="<?php echo base_url(); ?>finance/index"><img alt="Project: HumbleFinance" src="<?php static_image(); ?>products/finance-icon.jpg" /></a>
+	</span>
+	
+	<p>
+    <a href="<?php echo base_url(); ?>finance/index">HumbleFinance</a> 
+	is an HTML5 data visualization tool written as a demonstration
+    of interactive graphing in HTML5.  It is similar to the Flash tool on 
+    <a href="http://finance.google.com/">http://finance.google.com/</a>.  The
+    tool itself is written entirely in JavaScript, using the Prototype and Flotr 
+    libraries.  It can be used to display any two 2-D data sets of real 
+    numerical data which share an axis.  For best results use FireFox, Chrome, 
+    or Safari.
+    </p>
+</div>
+
+<h2 class="project-title"><a href="<?php echo base_url(); ?>transcriber/index">HumbleTranscriber</a></h2>
+
+<div class="project-summary">
+	
+    <span class="icon">
+        <a href="<?php echo base_url(); ?>transcriber/index"><img alt="Project: HumbleTranscriber, HTML5 Transcription Tool" src="<?php static_image(); ?>products/transcriber-icon.jpg" /></a>
+    </span>
+    
+	<p>
+    <a href="<?php echo base_url(); ?>transcriber/index">HumbleTranscriber</a> 
+	is an HTML5 video transcription application designed with ease of use in mind.  
+	It allows the user to do line-by-line transcription of audio or video, edit
+	existing transcriptions, and create translations.  Developers can easily
+	extend HumbleTranscriber to create a new application as well as integrate
+	the tool with existing applications.
+	</p>
+	
+	<p>
+	Written entirely in JavaScript, the tool provides several players including 
+	JW Player, YouTube, and HTML5.  It also exposes an AJAX based API for 
+	integration with a server.  Visit the <a href="<?php echo base_url(); ?>transcriber/demo">demo</a>
+	or check out LINK HERE to see a full transcription application in action.
+	</p>
+</div>
+
+<h2 class="project-title"><a href="<?php echo base_url(); ?>helloreddit/home">HelloReddit</a></h2>
+
+<div class="project-summary">
+	
+	<span class="icon" style="margin-top: -20px;">
+		<a href="<?php echo base_url(); ?>helloreddit/home"><img alt="Project: HelloReddit" src="<?php static_image(); ?>products/helloreddit-icon.jpg" /></a>
+	</span>
+	
+	<p>
+		<a href="<?php echo base_url(); ?>helloreddit/home">HelloReddit</a>
+        is a mobile BlackBerry app for the website <a href="http://www.reddit.com/">Reddit</a>.
+        The app lets the user browse Reddit, vote on articles, and 
+        submit pictures to <a href="http://www.imgur.com/">imgur</a>.  HelloReddit
+        is open source, licensed under the MIT License.
+        You can buy the app <a href="<?php echo base_url(); ?>helloreddit/buy">here</a>
+        or download the source <a href="<?php echo base_url(); ?>helloreddit/source">here</a>.
+	</p>
+</div>
+
