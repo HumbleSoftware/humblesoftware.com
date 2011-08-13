28d7afe62f28ddcfa9d72a85a1b42f54e7075f5f
Carl Sutherland <carl@humblesoftware.com>
2011-08-13 09:20:45 -0400

    Updated initial home.

--- application/views/home.php
+++ application/views/home.php
@@ -1,68 +1,68 @@
 <p class="summary">
 Welcome to humble software development.  We provide software design 
 and development services, as well as maintain several projects.  
-<a href="<?php echo base_url(); ?>contact">Contact us</a> or
+<a href="<?php echo base_url(); ?>contact">Contact</a> us or
  look around to learn more.
 </p>
 
 <div id="homeServices">
     <h3>Our expertise include:</h3>
     <ul>
-        <li>Web app development in JavaScript and HTML5</li>
-        <li>Custom dynamic data visualization in Canvas and SVG</li>
-        <li>Mobile web app development for Android and iOS</li>
-        <li>Data mining and information retrieval backends</li>
+        <li>JavaScript/HTML5 Application Development</li>
+        <li>Dynamic Data Visualization in Canvas and SVG</li>
+        <li>Search/Information Retrieval Backends</li>
     </ul>
 </div>
 
 <div id="homeSummary">
 	<div id="homeProjects" class="homeBox">
-		<h3>Products:</h3>
-        <div class="projectItem">
-            <span class="title">
-                <a href="<?php echo base_url(); ?>helloreddit/home">HelloReddit</a>
-            </span>
-            <span class="icon">
-                <a href="<?php echo base_url(); ?>helloreddit/home"><img alt="Project: HelloReddit" src="<?php ?>img/products/helloreddit-icon.jpg" /></a>
-            </span>
-            <span class="description">
-                <a href="<?php echo base_url(); ?>helloreddit/home">HelloReddit</a>
-				is a mobile BlackBerry app for the website <a href="http://www.reddit.com/">Reddit</a>.
-				The app lets the user browse Reddit, vote on articles, and 
-				submit pictures to <a href="http://www.imgur.com/">imgur</a>.  HelloReddit
-				is open source, licensed under the MIT License.
-				You can buy the app <a href="<?php echo base_url(); ?>helloreddit/buy">here</a>
-				or download the source <a href="<?php echo base_url(); ?>helloreddit/source">here</a>.
-            </span>
-        </div>
+
+		<h3>Projects:</h3>
+
 		<div class="projectItem">
 			<span class="title">
 				<a href="<?php echo base_url(); ?>finance/index">HumbleFinance</a>
 			</span>
             <span class="icon">
-                <a href="<?php echo base_url(); ?>finance/index"><img alt="Project: HumbleFinance" src="<?php ?>img/products/finance-icon.jpg" /></a>
+                <a href="<?php echo base_url(); ?>finance/index"><img alt="Project: HumbleFinance" src="<?php static_image(); ?>products/finance-icon.jpg" /></a>
 			</span>
 			<span class="description">
 				Dynamic charts with HTML5, Canvas, and Flotr for Prototype.  
 				<a href="<?php echo base_url(); ?>finance/index">HumbleFinance</a>
-				is an OpenSource project which can display any two 2-D numeric 
-				data sets which share an axis.
+				is an OpenSource project which can display two 2-D numeric 
+				datasets sharing an axis.
 			</span>
 		</div>
-	</div>
-	
-	<div id="homePortfolio" class="homeBox">
-		<h3>Portfolio:</h3>
-		<div class="portfolioItem">
-			<span class="icon"><a href="<?php echo base_url(); ?>portfolio/geradesignz"><img alt="Portfolio: MOCA" src="<?php ?>img/portfolio/moca-icon.jpg" /></a></span>
-			<span class="title"><a href="<?php echo base_url(); ?>portfolio/geradesignz">Moca</a></span>
-			<span class="description">An information retrieval system used by linguists for corpus analysis.</span>
+
+        <div class="projectItem">
+            <span class="title">
+                <a href="<?php echo base_url(); ?>movemytaxes/">Move My Taxes</a>
+            </span>
+            <span class="icon">
+                <a href="<?php echo base_url(); ?>helloreddit/home"><img alt="Project: HelloReddit" src="<?php static_image(); ?>products/movemytaxes-icon.jpg" /></a>
+            </span>
+            <span class="description">
+                Move My Taxes was created for the <a href="http://www.datavizchallenge.org" alt="DataVizChallenge">
+                Data Viz Challenge</a>.
+            </span>
 		</div>
-		<div class="portfolioItem">
-			<span class="icon"><a href="<?php echo base_url(); ?>portfolio/geradesignz"><img alt="Portfolio: EMI EMRE" src="<?php ?>img/portfolio/emi-icon.png" /></a></span>
-			<span class="title"><a href="<?php echo base_url(); ?>portfolio/emre">EMRE</a></span>
-			<span class="description">A search engine built to market the EMI music catelog.</span>
-		</div>
+
+        <div class="projectItem">
+            <span class="title">
+                <a href="<?php echo base_url(); ?>helloreddit/home">HelloReddit</a>
+            </span>
+            <span class="icon">
+                <a href="<?php echo base_url(); ?>helloreddit/home"><img alt="Project: HelloReddit" src="<?php echo static_image(); ?>products/helloreddit-icon.jpg" /></a>
+            </span>
+            <span class="description">
+                A mobile BlackBerry app for browsing <a href="http://www.reddit.com/">Reddit</a>.
+                It is OpenSource and written in Java.
+            </span>
+        </div>
+
 	</div>
 	<div class="footer"></div>
+</div>
+
+    </div>
 </div>
