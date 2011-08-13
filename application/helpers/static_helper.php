34bcdff587879a28a2bcfd791cf3054f6b0c719f
Carl Sutherland <carl@humblesoftware.com>
2011-08-13 09:22:02 -0400

    Added helper for static files.

---
+++ application/helpers/static_helper.php
@@ -0,0 +1,13 @@
+<?php
+function static_js() {
+    $CI =& get_instance();
+    echo $CI->config->item('static_js');
+}
+function static_css() {
+    $CI =& get_instance();
+    echo $CI->config->item('static_css');
+}
+function static_image() {
+    $CI =& get_instance();
+    echo $CI->config->item('static_images');
+}
