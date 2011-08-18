bbced067c6080811cd364cb58eb8d622ad11a9ea
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 19:10:54 -0400

    Added form validation config for contact form.

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -1,11 +1,38 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+    array(
+      'field' => '',
+      'label' => '',
+      'rules' => ''
+    )
+*/
 $config = array(
-  'recaptchademo/index' => array(
+  'contact/index' => array(
     array(
       'field' => 'recaptcha_response_field',
       'label' => 'lang:recaptcha_field_name',
-      'rules' => 'required|callback_check_captcha'
+      'rules' => 'required|callback__check_captcha'
+    ),
+    array(
+      'field' => 'name',
+      'label' => 'Name',
+      'rules' => ''
+    ),
+    array(
+      'field' => 'email',
+      'label' => 'Email',
+      'rules' => 'valid_email'
+    ),
+    array(
+      'field' => 'feedback',
+      'label' => 'Message',
+      'rules' => 'required'
+    ),
+    array(
+      'field' => 'subject',
+      'label' => 'Subject',
+      'rules' => ''
     )
   )
 );
