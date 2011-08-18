7ffae5a9fee223bfe5b7a1cea5b44c681eee27ab
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 19:12:11 -0400

    Added contact thank you view.

---
+++ application/views/contact_thankyou.php
@@ -0,0 +1,10 @@
+<div class="contact">
+<h3>Contact:</h3>
+
+<span class="label">Email</span><span id="contactEmail">carl [at] this domain name . com</span>
+
+<h3 class="feedback"><a name="Feedback"></a>Feedback:</h3>
+<p>
+Thank you for your feedback<?php if ($name) echo ", {$name}"; ?>.
+</p>
+</div>
