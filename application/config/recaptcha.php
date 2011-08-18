67753fc3fd84b29456735208f1f8f61196a35968
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 12:07:23 -0400

    Added recaptcha config.

---
+++ application/config/recaptcha.php
@@ -0,0 +1,10 @@
+<?
+$config['recaptcha'] = array(
+    'public'                        => '6LdfGscSAAAAAFiyO5s9-A-M9FU3n7Rg8_wJXf1U',
+    'private'                       => '6LdfGscSAAAAAJKXwxXmKPbEoi1Ec7JNRBu6Mm4r',
+    'RECAPTCHA_API_SERVER'          => 'http://www.google.com/recaptcha/api',
+    'RECAPTCHA_API_SECURE_SERVER'   => 'https://www.google.com/recaptcha/api',
+    'RECAPTCHA_VERIFY_SERVER'       => 'www.google.com',
+    'RECAPTCHA_SIGNUP_URL'          => 'https://www.google.com/recaptcha/admin/create',
+    'theme'                         => 'white'
+); 
