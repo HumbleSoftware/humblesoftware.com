fd52379bfbeaf97a5a8e0b2e42fb7c2af2d49fa5
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 19:11:27 -0400

    Integrated form and validation helpers with contact.php.

--- application/views/contact.php
+++ application/views/contact.php
@@ -4,14 +4,28 @@
 
 <h3 class="feedback"><a name="Feedback"></a>Feedback:</h3>
 
-<form method="post" action="<?php echo base_url(); ?>contact/send">
+<div class="errors">
+<?php echo validation_errors(); ?>
+</div>
+
+<?php echo form_open('contact'); ?>
+    <input type="hidden" name="action" value="feedback" />
 	<fieldset>
-		<input type="hidden" name="action" value="feedback" />
-		<div><label for="name">Name:</label><input type="text" id="name" name="name" /></div>
-		<div><label for="email">Email:</label><input type="text" id="email" name="email" /></div>
-		<div><label for="subject">Subject:</label><input id="subject" type="text" <?php if (isset($subject)) { echo "value=\"{$subject}\" "; } ?>name="subject" /></div>
-		<label for="feedback">Message:</label>
-		<textarea id="feedback" name="feedback" rows="21" cols="125"></textarea>
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" value="<?php echo set_value('name'); ?>"/>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email" value="<?php echo set_value('email'); ?>" /></div>
+        <div>
+            <label for="subject">Subject:</label>
+            <input id="subject" type="text" name="subject" value="<?php echo set_value('subject', (isset($subject) ? $subject : '')); ?>" />
+        </div>
+        <div>
+            <label for="feedback">Message:</label>
+            <textarea id="feedback" name="feedback" rows="21" cols="125"><?php echo set_value('feedback'); ?></textarea>
+        </div>
         <?php echo $recaptcha; ?>
 		<div><input id="submit" type="submit" name="submit" value="Send Feedback" /></div>
     </fieldset>
