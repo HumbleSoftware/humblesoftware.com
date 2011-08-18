aff1460cbc722d13d7d5d6807958c8576dce915e
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 19:24:08 -0400

    Added recaptcha and form validation to the contact controller.

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -14,27 +14,57 @@ class Contact extends CI_Controller {
     }
 	public function index()
 	{
-        $this->load->view('template', array(
-            'title' => 'humble software development - contact',
-            'page' => 'contact'
-        ));
-	}
-    public function send()
-    {
-        $this->load->helper(array(
-            'email'
-        ));
+        $helpers = array(
+            'email',
+            'form'
+        );
+        $libraries = array(
+            'recaptcha',
+            'form_validation'
+        );
 
+        $this->lang->load('recaptcha');
+        $this->load->helper($helpers);
+        $this->load->library($libraries);
+        $this->form_validation->set_message('_check_captcha', 'Captcha failed.');
+
+        if ($this->form_validation->run()) {
+            $this->send_feedback();
+            $this->load->view('template', array(
+                'title'     => 'humble software development - contact',
+                'page'      => 'contact_thankyou',
+                'name'      => $this->input->post('name')
+            ));
+        } else {
+            $this->load->view('template', array(
+                'title'     => 'humble software development - contact',
+                'page'      => 'contact',
+                'recaptcha' => $this->recaptcha->get_html()
+            ));
+        }
+	}
+
+    public function _check_captcha($val)
+    {
+        if ($this->recaptcha->check_answer($this->input->ip_address(),$this->input->post('recaptcha_challenge_field'),$val)) {
+            return TRUE;
+	    } else {
+            $this->form_validation->set_message('check_captcha',$this->lang->line('recaptcha_incorrect_response'));
+            return FALSE;
+	    }
+	}
+
+    protected function send_feedback()
+    {
         $name       = $this->input->post('name');
         $email      = $this->input->post('email');
         $subject    = $this->input->post('subject');
         $feedback   = $this->input->post('feedback');
-        $message    = "$name $email $subject $feedback";
+        $message    = "{$feedback}\r\n\r\n{$name}\r\n";
         $recipient  = $this->config->item('admin_email');
-        $headers    = "From: {$email}\r\n" .
-                      "Reply-To: {$email}\r\n";
+        $headers    = "From: feedback@humblesoftware.com\r\n";
 
-        if (!valid_email($email)) return false;
+        if ($email) $headers .= "Reply-To: {$email}\r\n";
 
         mail ($recipient, $subject, $message, $headers);
     }
