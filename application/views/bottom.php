53a20d188bcdec2ef8896c755682ccc4e1f8776b
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 20:30:01 -0400

    Removed space.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,8 +1,5 @@
 </div> 
 </body>
-
-<!-- scripts -->
 <script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-
 </html>
