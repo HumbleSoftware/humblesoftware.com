5868e683e455dbafcae50fc02afc2291ed2a5552
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 20:29:19 -0400

    Added google CND api jQuery include.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -2,5 +2,7 @@
 </body>
 
 <!-- scripts -->
+<script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 
 </html>
