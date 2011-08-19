11eb1b03606d8ecb52f3c285ff327345efdd7d18
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 20:35:10 -0400

    Added JS includes loop.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -3,4 +3,7 @@
 <script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script src="<?php static_lib(); ?>google-code-prettify/prettify.js"></script>
+<?php if (isset($includes)): foreach ($includes->getJS() as $script) { ?>
+    <script src="<?php static_js(); echo $script; ?>"></script>
+<?php } endif; ?>
 </html>
