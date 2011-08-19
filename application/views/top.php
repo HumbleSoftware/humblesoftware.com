745f98bc62ae1aa491727e364c41263c78871a6f
Carl Sutherland <carl@humblesoftware.com>
2011-08-18 20:33:19 -0400

    Added prettify library.

--- application/views/top.php
+++ application/views/top.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="icon" type="image/gif" href="<?php static_image(); ?>favicon.gif" />
     <link rel="stylesheet" type="text/css" href="<?php static_css(); ?>styles.css" />
+    <link rel="stylesheet" type="text/css" href="<?php static_lib(); ?>google-code-prettify/prettify.css" />
 </head>
 
 <body>
