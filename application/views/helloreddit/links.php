8d10d114b0f83eeacc3943a1217962f887aea5c3
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 19:08:00 -0400

    Updated link to feedback.

--- application/views/helloreddit/links.php
+++ application/views/helloreddit/links.php
@@ -5,7 +5,7 @@
     <li><a href="<?php echo base_url(); ?>helloreddit/features">Features</a></li>
     <li><a href="https://github.com/HumbleSoftware/HelloReddit/downloads"><b>Get It!</b></a></li>
     <li><a href="https://github.com/HumbleSoftware/HelloReddit">Source Code</a></li>
-    <li><a href="<?php echo base_url(); ?>contact?subject=HelloReddit%20Feedback#Feedback">Feedback</a></li>
+    <li><a href="<?php echo base_url(); ?>helloreddit/feedback">Feedback</a></li>
   </ul>
 <div id="links-footer"></div>
 </div>
