41b7d96a902e0a868649f70c3b9aa9a462d7b756
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 16:14:55 -0400

    Broke out feedback thank you.

--- application/views/contact_thankyou.php
+++ application/views/contact_thankyou.php
@@ -3,8 +3,4 @@
 
 <span class="label">Email</span><span id="contactEmail">carl [at] this domain name . com</span>
 
-<h3 class="feedback"><a name="Feedback"></a>Feedback:</h3>
-<p>
-Thank you for your feedback<?php if ($name) echo ", {$name}"; ?>.
-</p>
-</div>
+<?php $this->load->view('thankyou.php'); ?>
---
+++ application/views/thankyou.php
@@ -0,0 +1,5 @@
+<h3 class="feedback"><a name="Feedback"></a>Feedback:</h3>
+<p>
+Thank you for your feedback<?php if ($name) echo ", {$name}"; ?>.
+</p>
+</div>
