95ff334b944a6c703b99920f68183c0deb4ff3a3
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 18:53:18 -0400

    Removed old shit - buy download, paypal donate integration.

--- application/views/helloreddit/old/HelloRedditBuyButton.php
+++
@@ -1,8 +0,0 @@
-<div class="buyContainer">
-    <div class="get">
-    <a href="download">Get it!</a>
-    </div>
-    <div class="donateButton">
-    <?php include 'DonateButton.xhtml'; ?>
-    </div>
-</div>
