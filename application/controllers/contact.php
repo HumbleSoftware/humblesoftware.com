f1dd236a71125aa981537ebdd3e0a1f91c7ad6c0
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 15:42:48 -0400

    Updated to use feedback library.

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -14,22 +14,10 @@ class Contact extends CI_Controller {
     }
 	public function index()
 	{
-        $helpers = array(
-            'email',
-            'form'
-        );
-        $libraries = array(
-            'recaptcha',
-            'form_validation'
-        );
+        $this->load->library('feedback');
 
-        $this->lang->load('recaptcha');
-        $this->load->helper($helpers);
-        $this->load->library($libraries);
-        $this->form_validation->set_message('_check_captcha', 'Captcha failed.');
-
-        if ($this->form_validation->run()) {
-            $this->send_feedback();
+        if ($this->feedback->validate()) {
+            $this->feedback->send();
             $this->load->view('template', array(
                 'title'     => 'humble software development - contact',
                 'page'      => 'contact_thankyou',
@@ -46,26 +34,6 @@ class Contact extends CI_Controller {
 
     public function _check_captcha($val)
     {
-        if ($this->recaptcha->check_answer($this->input->ip_address(),$this->input->post('recaptcha_challenge_field'),$val)) {
-            return TRUE;
-	    } else {
-            $this->form_validation->set_message('check_captcha',$this->lang->line('recaptcha_incorrect_response'));
-            return FALSE;
-	    }
-	}
-
-    protected function send_feedback()
-    {
-        $name       = $this->input->post('name');
-        $email      = $this->input->post('email');
-        $subject    = $this->input->post('subject');
-        $feedback   = $this->input->post('feedback');
-        $message    = "{$feedback}\r\n\r\n{$name}\r\n";
-        $recipient  = $this->config->item('admin_email');
-        $headers    = "From: feedback@humblesoftware.com\r\n";
-
-        if ($email) $headers .= "Reply-To: {$email}\r\n";
-
-        mail ($recipient, $subject, $message, $headers);
+        return $this->feedback->check_captcha($val);
     }
 }
