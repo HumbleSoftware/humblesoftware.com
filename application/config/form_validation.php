923e0e23d8612456597c2d31fbba58426b3c3747
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 15:49:42 -0400

    Removed old form validation rules.

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -8,31 +8,8 @@
     )
 */
 $config = array(
-  'contact/index' => array(
-    array(
-      'field' => 'recaptcha_response_field',
-      'label' => 'lang:recaptcha_field_name',
-      'rules' => 'required|callback__check_captcha'
-    ),
-    array(
-      'field' => 'name',
-      'label' => 'Name',
-      'rules' => ''
-    ),
-    array(
-      'field' => 'email',
-      'label' => 'Email',
-      'rules' => 'valid_email'
-    ),
-    array(
-      'field' => 'feedback',
-      'label' => 'Message',
-      'rules' => 'required'
-    ),
-    array(
-      'field' => 'subject',
-      'label' => 'Subject',
-      'rules' => ''
-    )
+/*
+  'feedback/validate' => array(
   )
+ */
 );
