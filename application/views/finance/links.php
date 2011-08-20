2d7a21c73e3fdb0c663917c2c1f0bc6ee619319f
Carl Sutherland <carl@humblesoftware.com>
2011-08-20 11:05:05 -0400

    Initial finance links.

---
+++ application/views/finance/links.php
@@ -0,0 +1,20 @@
+<h2 class="title">
+    HumbleFinance: <span class="subTitle">Dynamic Charts with HTML5, Canvas, and Flotr for Prototype</span>
+</h2>
+
+<div id="subLinks">
+  <ul>
+  <li style="float: left;" <?php echo ($financePage == 'index' ? ' class="currentPage"' : ''); ?>>
+    <a href="<?php echo base_url(); ?>finance/index"><span>demo</span></a>
+  </li>
+  <li<?php echo ($financePage == 'documentation' ? ' class="currentPage"' : ''); ?>>
+    <a href="<?php echo base_url(); ?>finance/documentation"><span>documentation</span></a>
+  </li>
+  <li<?php echo ($financePage == 'source' ? ' class="currentPage"' : ''); ?>>
+    <a href="<?php echo base_url(); ?>finance/source"><span>source</span></a>
+  </li>
+  <li>
+    <a href="<?php echo base_url(); ?>contact?subject=HumbleFinance%20Feedback#Feedback"><span>feedback</span></a>
+  </li>
+</ul>
+</div>
