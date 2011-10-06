169880768644cfc08efa4340bb179123243444b7
Carl Sutherland <carl@humblesoftware.com>
2011-10-06 16:30:48 -0400

    Fixed broken markup.

--- application/views/demos/demos.php
+++ application/views/demos/demos.php
@@ -1,6 +1,6 @@
 <h2>Demos</h2>
 
 <ul>
-  <li><a href="<?php echo base_url(); ?>demos/trig">Trig - Using Canvas</a>
-  <li><a href="<?php echo base_url(); ?>demos/trig_d3">Trig - Using D3</a>
+  <li><a href="<?php echo base_url(); ?>demos/trig">Trig - Using Canvas</a></li>
+  <li><a href="<?php echo base_url(); ?>demos/trig_d3">Trig - Using D3</a></li>
 </ul>
