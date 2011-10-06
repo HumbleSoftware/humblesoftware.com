17604c10504fba61dfd65752a93237bee726b305
Carl Sutherland <carl@humblesoftware.com>
2011-10-06 16:29:39 -0400

    Added trig and d3 demo to controller.

--- application/controllers/demos.php
+++ application/controllers/demos.php
@@ -27,4 +27,29 @@ class Demos extends CI_Controller {
             'page' => 'demos/demos'
         ));
     }
+    public function trig()
+    {
+        $this->includes->css('demos.css');
+        $this->includes->js('demos/Trig.js');
+        $this->includes->js('demos/TrigDemo.js');
+
+        $this->load->view('template', array(
+            'includes' => $this->includes,
+            'title' => 'humble software development - demos',
+            'page' => 'demos/trig'
+        ));
+    }
+    public function trig_d3()
+    {
+        $this->includes->css('demos.css');
+        $this->includes->css('demos/trig-d3.css');
+        $this->includes->js('d3.min.js');
+        $this->includes->js('demos/trig-d3.js');
+
+        $this->load->view('template', array(
+            'includes' => $this->includes,
+            'title' => 'humble software development - demos',
+            'page' => 'demos/trig-d3'
+        ));
+    }
 }
