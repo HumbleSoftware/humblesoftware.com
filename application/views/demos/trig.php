c76b85cc0f138f617123d7b762aa588255e91ac1
Carl Sutherland <carl@humblesoftware.com>
2011-10-06 16:26:33 -0400

    Moved trig demo to trig.php.

---
+++ application/views/demos/trig.php
@@ -0,0 +1,24 @@
+<h2>Trig Demo</h2>
+
+<p id="summary" style="text-align: center;">
+    The first thing that should be shown in any Trigonometry class, done in
+	Canvas.  Compare to: <a href="http://www.reddit.com/tb/bnzgx">
+	http://www.reddit.com/tb/bnzgx</a>. 
+</p>
+
+<div id="canvas-container" style="width: 800px; height: 300px;">
+    <canvas id="sineCanvas"  style="width: 800px; height: 300px;"></canvas>
+</div>
+
+<p></p>
+
+<h3>
+    Listing <span class="file">Trig.js</span>:
+    <span class="snippetShow">
+        <a id="snippetShowTwo" onclick="toggleSnippet($('snippetShowTwo'), $('snippetTwo'));">(hide)</a>
+    </span>
+</h3>
+
+<div id="snippetTwo" class="snippet">
+    <code class="prettyprint"><?php echo htmlentities(file_get_contents('../static/js/demos/Trig.js', FILE_USE_INCLUDE_PATH)); ?></code>
+</div>
