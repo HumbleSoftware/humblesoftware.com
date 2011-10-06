23c800496dc89d5dd080cfffe56a527c2a914145
Carl Sutherland <carl@humblesoftware.com>
2011-10-06 17:54:34 -0400

    Added optional content parameter.

--- application/views/listing.php
+++ application/views/listing.php
@@ -7,6 +7,8 @@
   </h3>
 
   <div class="snippet">
-    <code class="prettyprint"><?php echo htmlentities(file_get_contents($file, FILE_USE_INCLUDE_PATH)); ?></code>
+    <code class="prettyprint"><?php
+echo (isset($listingContent) ? $listingContent : htmlentities(file_get_contents($file, FILE_USE_INCLUDE_PATH)));
+?></code>
   </div>
 </div>
