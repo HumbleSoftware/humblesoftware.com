5bb53a4d8da2b3faf9fa7012223744106e77ea54
Carl Sutherland <carl@humblesoftware.com>
2011-10-06 16:25:14 -0400

    Added trig-d3 demo.

---
+++ application/views/demos/trig-d3.php
@@ -0,0 +1,23 @@
+<h2>Trig Demo - D3</h2>
+
+<p id="summary" style="text-align: center;">
+    The first thing that should be shown in any Trigonometry class, done in
+	d3.js  Compare to: <a href="http://www.reddit.com/tb/bnzgx">
+	http://www.reddit.com/tb/bnzgx</a>. 
+</p>
+
+<div id="trig"></div>
+
+<div class="listing">
+  <h3>Listing
+    <span class="file">trig-d3.js</span>:
+    <span>
+      <a class="listing-hide">(hide)</a>
+    </span>
+  </h3>
+
+  <div class="snippet">
+    <code class="prettyprint"><?php echo htmlentities(file_get_contents('../static/js/demos/trig-d3.js', FILE_USE_INCLUDE_PATH)); ?></code>
+  </div>
+</div>
+
