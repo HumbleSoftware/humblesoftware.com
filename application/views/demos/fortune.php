d4da7916400dc42dd6ae5d1e9706eb421e6f2b2d
Carl Sutherland <carl@humblesoftware.com>
2011-10-11 17:59:15 -0400

    Added year labels.

--- application/views/demos/fortune.php
+++ application/views/demos/fortune.php
@@ -8,6 +8,21 @@
   <div class="control profit">Profit</div>
 </div>
 
+<div class="labels">
+  <div class="label">1955</div>
+  <div class="label">1960</div>
+  <div class="label">1965</div>
+  <div class="label">1970</div>
+  <div class="label">1975</div>
+  <div class="label">1980</div>
+  <div class="label">1985</div>
+  <div class="label">1990</div>
+  <div class="label">1995</div>
+  <div class="label">2000</div>
+  <div class="label">2005</div>
+  <div class="label">2010</div>
+</div>
+
 <div class="company"></div>
 
 </div>
