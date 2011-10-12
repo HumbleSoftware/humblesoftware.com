8e90695318e70fd8bdaba215643c5e55b140470d
Carl Sutherland <carl@humblesoftware.com>
2011-10-12 13:36:08 -0400

    Updated markup adding description.

--- application/views/demos/fortune.php
+++ application/views/demos/fortune.php
@@ -1,34 +1,51 @@
+<div class="fortune">
+
 <h2>Fortune 500 Demo</h2>
 
+<p id="summary" style="text-align: center;">
+As seen on <a href="http://fathom.info/fortune500/">Fathom</a>, done in Canvas and JavaScript.  Best viewed in Chrome or Firefox.  Click to select points; navigate with arrow keys.
+</p>
+
 <div id="chart" class="chart">
-
-<div class="controls">
-  <div class="label">showing:</div>
-  <div class="control rank selected">Rank</div>
-  <div class="control revenue">Revenue</div>
-  <div class="control profit">Profit</div>
+  <div class="controls">
+    <div class="label">showing:</div>
+    <div class="control rank selected">Rank</div>
+    <div class="control revenue">Revenue</div>
+    <div class="control profit">Profit</div>
+  </div>
+  <div class="inflation">
+    <input id="inflation-checkbox" type="checkbox"></input>
+    <label for="inflation-checkbox">inflation</label>
+  </div>
+  <div class="labels">
+    <div class="label">1955</div>
+    <div class="label">1960</div>
+    <div class="label">1965</div>
+    <div class="label">1970</div>
+    <div class="label">1975</div>
+    <div class="label">1980</div>
+    <div class="label">1985</div>
+    <div class="label">1990</div>
+    <div class="label">1995</div>
+    <div class="label">2000</div>
+    <div class="label">2005</div>
+    <div class="label">2010</div>
+  </div>
+  <div class="company"></div>
 </div>
 
-<div class="inflation">
-  <input id="inflation-checkbox" type="checkbox"></input>
-  <label for="inflation-checkbox">adjust for inflation</label>
-</div>
+<h3>About</h3>
 
-<div class="labels">
-  <div class="label">1955</div>
-  <div class="label">1960</div>
-  <div class="label">1965</div>
-  <div class="label">1970</div>
-  <div class="label">1975</div>
-  <div class="label">1980</div>
-  <div class="label">1985</div>
-  <div class="label">1990</div>
-  <div class="label">1995</div>
-  <div class="label">2000</div>
-  <div class="label">2005</div>
-  <div class="label">2010</div>
-</div>
-
-<div class="company"></div>
+<p>
+I thought the Fathom Fortune 500 vis, done in Java, was excellent.  This version was created to demonstrate 
+that 80,000 data points could be visualized with JavaScript using modern browser features.  This 
+visualization uses JavaScript and Canvas.
+</p>
+<h3>Vitals</h3>
+<ul>
+  <li>JavaScript &amp; Canvas</li>
+  <li>237KB of Data</li>
+  <li>Tested in Chrome, Firefox, Android Webkit and Firefox Mobile.</li>
+<ul>
 
 </div>
