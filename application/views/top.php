2aac99c1993115913ab27f3641da8d38d3a9db7b
Carl Sutherland <carl@humblesoftware.com>
2011-10-29 15:17:49 -0400

    Updated google analytics snipped to async.

--- application/views/top.php
+++ application/views/top.php
@@ -9,6 +9,19 @@
 <?php if (isset($includes)): foreach ($includes->getCSS() as $stylesheet) { ?>
     <link rel="stylesheet" type="text/css" href="<?php static_css(); echo $stylesheet; ?>"></link>
 <?php } endif; ?>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-15129441-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 </head>
 
 <body>
