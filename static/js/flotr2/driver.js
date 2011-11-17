ec76d671296d5c77b36c390f63879d78a8322c0b
Carl Sutherland <carl@humblesoftware.com>
2011-11-17 14:44:55 -0500

    Added flotr2 source files.

---
+++ static/js/flotr2/driver.js
@@ -0,0 +1,48 @@
+$().ready(function () {
+
+  Examples = new Flotr.Examples({
+    node : document.getElementById('examples'),
+    thumbPadding : 150
+  });
+
+  var
+    examples  = $('#examples').find('.flotr-examples'),
+    thumbs    = examples.find('.flotr-examples-thumbs'),
+    offset    = 128,
+    page      = $(window);
+
+  $(document)
+    .scroll(handleScrollSize);
+  page
+    .resize(handleResizeSize);
+
+  function handleScrollSize () {
+    if (examples.hasClass('flotr-examples-large') || examples.hasClass('flotr-examples-medium')) {
+      if (_.isNull(offset)) {
+        offset = parseInt(thumbs.css('top'));
+        if (_.isNaN(offset)) {
+          offset = 0;
+        }
+      }
+      var
+        scrollTop = $(document).scrollTop(),
+        top = Math.max(0, offset - scrollTop);
+      thumbs.css({
+        top : top,
+        height : page.height() - 22 - top
+      });
+      console.log(top);
+      Examples.options.thumbPadding = top + 22
+    }
+  }
+
+  function handleResizeSize () {
+    if (examples.hasClass('flotr-examples-large') || examples.hasClass('flotr-examples-medium')) {
+      handleScrollSize();
+    } else {
+      thumbs.css({
+        top : 'auto'
+      });
+    }
+  }
+});
