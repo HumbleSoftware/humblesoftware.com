3fd0194d4138c7ae7f37936c74357aef20d37b8a
Carl Sutherland <carl@humblesoftware.com>
2011-11-17 12:56:08 -0500

    Removed type attribute.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,6 +1,6 @@
 </div> 
 </body>
-<script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw" type="text/javascript"></script>
+<script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script src="<?php static_lib(); ?>google-code-prettify/prettify.js"></script>
 <script src="<?php static_js(); ?>HSD.js"></script>
