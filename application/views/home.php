46d92cb37f61c6b93c8ccdef04e07fe0b190aea7
Carl Sutherland <carl@humblesoftware.com>
2012-01-16 16:27:46 -0500

    Updated home page.

--- application/views/home.php
+++ application/views/home.php
@@ -1,18 +1,11 @@
 <p class="summary">
 Welcome to humble software development.  We provide software design 
 and development services, as well as maintain several projects.  
+Services include JavaScript/HTML5 application development, Canvas/SVG
+dynamic data visualization, and search/information retrieval.
 <a href="<?php echo base_url(); ?>contact">Contact</a> us or
  look around to learn more.
 </p>
-
-<div id="homeServices">
-    <h3>Our expertise include:</h3>
-    <ul>
-        <li>JavaScript/HTML5 application development</li>
-        <li>Dynamic data visualization in Canvas and SVG</li>
-        <li>Search/information retrieval backends</li>
-    </ul>
-</div>
 
 <div id="homeSummary">
 	<div id="homeProjects" class="homeBox">
