30ef5405afa5f2cbeb7155fbb88d5087805ea1ad
Carl Sutherland <carl@humblesoftware.com>
2012-01-19 11:26:03 -0500

    Added flotr IE libs.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -3,6 +3,9 @@
 <script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="<?php static_lib(); ?>google-code-prettify/prettify.js"></script>
+<!--[if lt IE 9]>
+<script src="<?php static_js(); ?>flotr2/flotr2.ie.min.js"></script>
+<![endif]-->
 <script src="<?php static_js(); ?>HSD.js"></script>
 <?php if (isset($includes)): foreach ($includes->getJS() as $script) { ?>
     <script src="<?php static_js(); echo $script; ?>"></script>
