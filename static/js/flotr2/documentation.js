9d67d2d98ccda4bef8eb5ba3b9dd77a16fbdd74d
Carl Sutherland <carl@humblesoftware.com>
2012-01-30 20:32:24 -0500

    First pass at documentation.

--- static/js/flotr2/documentation.js
+++ static/js/flotr2/documentation.js
@@ -3,12 +3,46 @@ $(function () {
     Editor = Flotr.Examples.Editor;
 
   $.get('example', function (response) {
-    new Editor($('.editor.basic'), {
+    var
+      usageExample = $('.editor.usage');
+
+    new Editor($('.editor.usage'), {
+      noRun : ($.browser.msie && $.browser.msie < 9 ? true : false),
       example : response,
       type : 'html'
     });
+
+    $.get(HSD_BASE + 'static/lib/flotr2/js/DefaultOptions.js', function (response) {
+      new Editor($('.editor.api'), {
+        noRun : true,
+        example : response
+      });
+    });
   });
-  /**/
+
+  var
+    doc = $(document),
+    nav = $('#documentation-nav'),
+    offset = nav.offset(),
+    animation;
+
+  /*
+  nav.css({
+    position : 'fixed'
+  });
+
+  doc.scroll(function (e) {
+
+    var
+      scrollTop = doc.scrollTop(),
+      bottom = Math.max(offset.top - scrollTop, 10);
+
+    nav
+      .stop()
+      .animate({
+        top : top
+      });
+  });
 
   /*
   editor($('.editor.basic'), {
