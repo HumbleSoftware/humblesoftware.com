646b48e5f9825d8d2dcccefcc7bde5246a9906e3
Carl Sutherland <carl@humblesoftware.com>
2012-02-12 14:23:36 -0500

    Include jquery in hsd; remove google cdn.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -11,9 +11,10 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
-</script>
+</script><?php /*
 <script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+*/?>
 <script src="<?php static_js(); ?>hsd.js"></script>
 <!--[if lt IE 9]>
 <script src="<?php static_js(); ?>hsd-ie8.js"></script>
