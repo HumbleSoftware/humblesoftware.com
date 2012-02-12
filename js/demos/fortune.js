e37e4dc4ad293883c73ef2d87c2cf27a3242d869
Carl Sutherland <carl@humblesoftware.com>
2012-02-12 10:30:09 -0500

    Updated to un-minified fortune.js.

--- js/demos/fortune.js
+++ js/demos/fortune.js
@@ -2,4 +2,451 @@
 // Fortune 500
 // Copyright 2011 Humble Software Development
 //
-(function(){function y(a){return a>1e3?"$"+Math.round(a/100)/10+"b":"$"+Math.round(10*a)/10+"m"}function z(a,b){var c=l[j],d=l[a],e=l[i],f=F500.names,g=F500.values,h=g.length,k=1-b,m,p,q,r,s,t,u;v.clearRect(0,0,n,o),v.beginPath();for(r=0;r<h;r++){p=g[r],m=p.length,q=f[r].length/4;for(s=0;s<m;s++)t=p[s][c],p[s][e]=u=b*p[s][d]+k*(p[s][e]||p[s][d]),v.moveTo(t-q,u),v.lineTo(t+q,u)}v.closePath(),v.stroke(),w!==null&&B(a,w,b)}function A(a,b,c,d){var e=l[j],f=l[a],g=F500.values,h=g.length,i,k,m,n;for(m=0;m<h;m++){k=g[m],i=k.length;if(d){for(n=0;n<i;n++)if(Math.abs(k[n][e]-b)<7&&k[n][f]==c)return{i:m,j:n}}else for(n=0;n<i;n++)if(Math.abs(k[n][e]-b)<7&&Math.abs(k[n][f]-c)<1)return{i:m,j:n}}}function B(a,b,d){var e=F500.names[b.i],f=Math.max(e.length/4,3),g=F500.values[b.i],h=l[j],k=l[i],m=l[a],n,o,p;v.save(),v.strokeStyle=c,v.lineWidth="2.5",v.beginPath();for(p=0;p<g.length;p++)n=g[p][h],o=d*g[p][m]+(1-d)*(g[p][k]||g[p][b.i]),v.moveTo(n-f,o),v.lineTo(n+f,o);v.moveTo(g[0][h],g[0][m]),v.closePath(),v.stroke(),v.restore(),D(b.i,b.j,d)}function C(a,b){var c=F500.values[a][b],d=F500.names[a],e='<div class="company-content"><div class="value name">'+d+"</div>",f;for(f in k)e+='<div class="data '+f+'">',e+='<div class="label">'+f+"</div>",e+='<div class="value">'+(m[f]?m[f](c[k[f]]):c[k[f]])+"</div>",e+="</div>";e+='</div><div class="company-arrow"></div>',u.html(e).show()}function D(a,b,c){var d=F500.values[a][b],e=l[x],f=l[i],g=d[l[j]],h=c*d[e]+(1-c)*(d[f]||0);u.css({left:g-u.width()/2,top:Math.round(h+4)})}function E(){function p(a){a?(w=a,C(a.i,a.j),z(x,1)):(w=null,u.hide(),z(x,1))}function q(){return!!$("#inflation-checkbox").attr("checked")}function r(a){var b=q(),c=[d,g,e],i;for(var j in c)if($(a.target).hasClass(c[j])){i=c[j];if(i===x)return;$(".controls .control").removeClass("selected"),$(a.target).addClass("selected"),b&&(i==g?i=h:i==e&&(i=f)),s(i);return}}function s(a){b=x,x=a,c=0,k||v()}function v(){if(!n){z(x,1);return}k=!0,clearTimeout(i),c?c+=.08:c=.08,c>1&&(c=1),z(x,(1+Math.sin(-Math.PI/2-c*Math.PI))/2),c<1?i=setTimeout(function(){v()},25):k=!1}var b=null,c=0,i=null,k=!1,m=$(a),n,o;F(),o=new Date,z(d,1),n=new Date-o<150?!0:!1,$(".controls .control").click(r),$("#inflation-checkbox").change(function(a){var b=q();b?x==g?s(h):x==e&&s(f):x==h?s(g):x==f&&s(e)}),$(window).keydown(function(a){if(w===null)return;var b=F500.values,c=b[w.i][w.j],e=c[l[j]],f=c[l[d]],g;switch(a.keyCode){case 37:e-=14;break;case 38:f--;break;case 39:e+=14;break;case 40:f++;break;default:return}return a.preventDefault(),g=A(d,e,f,!0),p(g),!1}),t.click(function(a){var b=t.offset(),c=a.pageX-b.left,d=a.pageY-b.top,e=A(x,c,d);p(e)})}function F(){t.attr("width",n).attr("height",o),v.strokeStyle=b,v.translate(0,-0.5),v.lineWidth="1px"}function G(){var a=o/Math.log(6e5),b=o/(2*Math.log(1e6)),c=k[g],d=k[e],f=k[j],h=F500.values,i=F500.inflation,l=o/2,m=h.length,n,p,q,r,s,t,u;for(t=0;t<m;t++){p=h[t],n=p.length;for(u=0;u<n;u++)q=p[u],r=q[f],q.push(Math.round(o-Math.log(q[c]-40)*a)),q.push(Math.round(o-Math.log(q[c]*i[r]-40)*a)),s=q[d],s<0?(q.push(Math.round(l+Math.log(-10*s)*b)),q.push(Math.round(l+Math.log(-10*s*i[r])*b))):(q.push(Math.round(l-Math.log(10*s)*b)),q.push(Math.round(l-Math.log(10*s*i[r])*b)))}}var a="#chart",b="rgba(72,99,160,.8)",b="rgba(72,72,72,.8)",b="rgba(72,72,72,.3)",b="rgba(181,192,217,1)",c="rgba(200,30,30,1)",d="rank",e="profit",f="iProfit",g="revenue",h="iRevenue",i="previous",j="year",k={year:0,rank:1,revenue:2,profit:3},l={year:0,rank:1,previous:4,revenue:5,iRevenue:6,profit:7,iProfit:8},m={year:function(a){return Math.round((a+.5)*57/800+1954)},revenue:y,profit:y},n=800,o=502,p=1954,q=2010,r=0,s=500,t=$(a).append("<canvas></canvas>").find("canvas"),u=$(".company"),v=t[0].getContext("2d"),w=null,x=d;E(),setTimeout(function(){G()},10)})();
+
+(function () {
+
+  var
+
+    ID_CHART  = '#chart',
+
+    COLOR     = 'rgba(72,99,160,.8)',
+    COLOR     = 'rgba(72,72,72,.8)',
+    COLOR     = 'rgba(72,72,72,.3)',
+    COLOR     = 'rgba(181,192,217,1)',
+    HIGHLIGHT = 'rgba(200,30,30,1)',
+    RANK      = 'rank',
+    PROFIT    = 'profit',
+    I_PROFIT  = 'iProfit',
+    REVENUE   = 'revenue',
+    I_REVENUE = 'iRevenue',
+    PREVIOUS = 'previous',
+    YEAR      = 'year',
+
+    D_MAP     = { // Data Map
+      year    : 0,
+      rank    : 1, 
+      revenue : 2,
+      profit  : 3
+    },
+
+    C_MAP     = { // Chart Map
+      year      : 0,
+      rank      : 1, 
+      previous  : 4,
+      revenue   : 5,
+      iRevenue  : 6,
+      profit    : 7,
+      iProfit   : 8
+    },
+
+    TRANSLATIONS = {
+      year    : function (v) { return Math.round((v + .5) * (2011 - 1954) / 800 + 1954) },
+      revenue : format,
+      profit  : format 
+    },
+
+    width     = 800,
+    height    = 502,
+    xmin      = 1954,
+    xmax      = 2010,
+    ymin      = 0,
+    ymax      = 500, 
+    vis       = $(ID_CHART).append('<canvas></canvas>').find('canvas'),
+    company   = $('.company'),
+    context   = vis[0].getContext('2d'),
+    selected  = null,
+    type      = RANK;
+
+  init();
+
+  function format (v) {
+    if (v > 1000) {
+      return '$'+(Math.round(v/100)/10)+'b'; 
+    } else {
+      return '$'+(Math.round(10 * v)/10)+'m'; 
+    }
+  }
+
+  /**
+   * Main draw routine.
+   *
+   * This routine draws about the lines representing the data, about 27500 
+   * points at once and represents the main critical section.  Previous
+   * drawn values are remembered and used, together with the increment
+   * paramenter, to render frames of animation.
+   *
+   * Optimizations include precalculating the data, array access vs. 
+   * refinement, and using a single path and stroke.
+   */
+  function draw (type, increment) {
+
+    var
+      year    = C_MAP[YEAR],
+      index   = C_MAP[type],
+      pIndex  = C_MAP[PREVIOUS],
+      names   = F500.names,
+      values  = F500.values,
+      length1 = values.length,
+      dec     = 1 - increment,
+      length2,
+      data,
+      nameWidth,
+      i, j,
+      x, y;
+
+    context.clearRect(0, 0, width, height);
+    context.beginPath();
+
+    for (i = 0; i < length1; i++) {
+
+      data      = values[i];
+      length2   = data.length;
+      nameWidth = names[i].length / 4;
+
+      for (j = 0; j < length2; j++) {
+        x = data[j][year];
+        data[j][pIndex] = y =
+          increment * data[j][index] + dec * (data[j][pIndex] || data[j][index]);
+        context.moveTo(x - nameWidth, y);
+        context.lineTo(x + nameWidth, y);
+      }
+    }
+
+    context.closePath();
+    context.stroke();
+
+    if (selected !== null)
+      drawCompany(type, selected, increment);
+  }
+
+  function search (type, x, y, exact) {
+
+    var
+      year    = C_MAP[YEAR],
+      index   = C_MAP[type],
+      values  = F500.values,
+      length1 = values.length,
+      length2,
+      data,
+      i, j;
+
+    for (i = 0; i < length1; i++) {
+
+      data    = values[i];
+      length2 = data.length;
+
+      if (exact) {
+        for (j = 0; j < length2; j++) {
+          if (Math.abs(data[j][year] - x) < 7 && data[j][index] == y) {
+            return {i : i, j : j};
+          }
+        }
+      } else {
+        for (j = 0; j < length2; j++) {
+          if (Math.abs(data[j][year] - x) < 7 && Math.abs(data[j][index] - y) < 1) {
+            return {i : i, j : j};
+          }
+        }
+      }
+    }
+  }
+
+  function drawCompany (type, selected, increment) {
+
+    var
+      name      = F500.names[selected.i],
+      nameWidth = Math.max(name.length / 4, 3),
+      values    = F500.values[selected.i],
+      year      = C_MAP[YEAR],
+      previous  = C_MAP[PREVIOUS],
+      typeIndex = C_MAP[type],
+      x, y,
+      i;
+
+    context.save();
+
+    context
+      .strokeStyle = HIGHLIGHT;
+    context
+      .lineWidth = '2.5';
+
+    context.beginPath();
+
+    for (i = 0; i < values.length; i++) {
+      x = values[i][year];
+      y = increment * values[i][typeIndex] + ( 1 - increment ) * (values[i][previous] || values[i][selected.i]);
+      context.moveTo(x - nameWidth, y);
+      context.lineTo(x + nameWidth, y);
+    }
+
+    context.moveTo(values[0][year], values[0][typeIndex]);
+
+    context.closePath();
+    context.stroke();
+    context.restore();
+
+    positionDisplay(selected.i, selected.j, increment);
+  }
+
+  function display (i, j) {
+
+    var
+      values  = F500.values[i][j],
+      name    = F500.names[i],
+      html    = '<div class="company-content"><div class="value name">' + name + '</div>',
+      key;
+
+    for (key in D_MAP) {
+      html += '<div class="data ' + key + '">';
+      html += '<div class="label">' + key + '</div>';
+      html += '<div class="value">' + (TRANSLATIONS[key] ? TRANSLATIONS[key](values[D_MAP[key]]) : values[D_MAP[key]]) + '</div>';
+      html += '</div>';
+    }
+
+    html += '</div><div class="company-arrow"></div>';
+
+    company.html(html).show();
+  }
+
+  function positionDisplay (i, j, increment) {
+
+    var
+      values  = F500.values[i][j],
+      index   = C_MAP[type],
+      prev    = C_MAP[PREVIOUS],
+      x       = values[C_MAP[YEAR]],
+      y       = increment * values[index] + ( 1 - increment ) * (values[prev] || 0);
+
+    company.css({
+      left : x - company.width() / 2,
+      top  : Math.round(y + 4)
+    });
+  }
+
+  function init () {
+
+    var
+      previous  = null,
+      increment = 0,
+      timeout   = null,
+      animating = false,
+      chart     = $(ID_CHART),
+      doAnimations,
+      start;
+
+    visualization();
+    
+    start = new Date();
+    draw(RANK, 1);
+    doAnimations = ((new Date()) - start < 150 ? true : false);
+
+    $('.controls .control').click(switchType);
+
+    $('#inflation-checkbox').change(function (e) {
+      var
+        inflation = getInflation();
+
+      if (inflation) {
+        if (type == REVENUE)
+          doType(I_REVENUE);
+        else if (type == PROFIT)
+          doType(I_PROFIT);
+      } else {
+        if (type == I_REVENUE)
+          doType(REVENUE);
+        else if (type == I_PROFIT)
+          doType(PROFIT);
+      }
+    });
+
+    $(window).keydown(function (e) {
+
+      if (selected === null)
+        return;
+
+      var
+        values  = F500.values,
+        data    = values[selected.i][selected.j],
+        x       = data[C_MAP[YEAR]],
+        y       = data[C_MAP[RANK]],
+        result;
+
+      switch (e.keyCode)
+      {
+        case 37 : x -= 14; break;
+        case 38 : y--; break;
+        case 39 : x += 14; break;
+        case 40 : y++; break;
+        default : return;
+      }
+
+      e.preventDefault();
+
+      result = search(RANK, x, y, true);
+      setSelected(result);
+
+      return false;
+    });
+
+    vis.click(function (e) {
+
+      var
+        position = vis.offset(),
+        x = e.pageX - position.left,
+        y = e.pageY - position.top,
+        result = search(type, x, y);
+
+      setSelected(result);
+    });
+
+    function setSelected (o) {
+      if (o) {
+        selected = o;
+        display(o.i, o.j);
+        draw(type, 1);
+      } else {
+        selected = null; 
+        company.hide();
+        draw(type, 1);
+      }
+    }
+
+    function getInflation () {
+      return !!$('#inflation-checkbox').attr('checked');
+    }
+
+    function switchType (e) {
+      var
+        inflation = getInflation(),
+        types = [RANK, REVENUE, PROFIT],
+        newType;
+
+      for (var i in types) {
+        if ($(e.target).hasClass(types[i])) {
+
+          newType = types[i];
+          if (newType === type) return;
+
+          $('.controls .control').removeClass('selected');
+          $(e.target).addClass('selected');
+
+          if (inflation) {
+            if (newType == REVENUE)
+              newType = I_REVENUE;
+            else if (newType == PROFIT)
+              newType = I_PROFIT;
+          }
+
+          doType(newType);
+
+          return;
+        }
+      }
+    }
+
+    function doType (t) {
+      previous = type;
+      type = t;
+      increment = 0;
+      if (!animating) animate();
+    }
+
+    function animate () {
+
+      if (!doAnimations) {
+        draw(type, 1);
+        return;
+      }
+
+      animating = true;
+      clearTimeout(timeout);
+
+      if (!increment) increment = .08;
+      else increment += .08;
+      if (increment > 1) increment = 1;
+
+      draw (type, (1 + Math.sin(-Math.PI / 2 - increment * Math.PI))/2);
+
+      if (increment < 1) {
+        timeout = setTimeout (function () {
+          animate();
+        }, 25);
+      } else {
+        animating = false;
+      }
+    }
+  }
+
+  function visualization () {
+
+    vis
+      .attr('width', width)
+      .attr('height', height);
+
+    context
+      .strokeStyle = COLOR;
+    context
+      .translate(0, -.5);
+    context
+      .lineWidth = '1px';
+  }
+
+  /**
+   * Precalculate revenue, price and inflation.
+   *
+   * Revenue and profit are stored as actual values (in millions).  This value
+   * is displayed to the user in the company popup.  To draw the data, these
+   * values need to be translated to their y coordinates.
+   *
+   * This translation is precalculated for speed at draw time and calculated
+   * client-side to keep the data size down.
+   *
+   * Run after first draw.
+   */
+  function calculatePoints () {
+
+    var
+      rBase     = height / Math.log(6e5),
+      pBase     = height / ( 2 * Math.log(1e6) ),
+      rIndex    = D_MAP[REVENUE],
+      pIndex    = D_MAP[PROFIT],
+      yIndex    = D_MAP[YEAR],
+      values    = F500.values,
+      inflation = F500.inflation,
+      half      = height / 2,
+      length1   = values.length,
+      length2,
+      data,
+      datum,
+      year,
+      v,
+      i, j;
+
+    for (i = 0; i < length1; i++) {
+
+      data = values[i];
+      length2 = data.length;
+
+      for (j = 0; j < length2; j++) {
+        datum = data[j];
+        year = datum[yIndex];
+        datum.push(Math.round(height - Math.log(datum[rIndex] - 40) * rBase));
+        datum.push(Math.round(height - Math.log(datum[rIndex] * inflation[year] - 40) * rBase));
+        v = datum[pIndex];
+        if (v < 0) {
+          datum.push(Math.round(half + Math.log(-10 * v) * pBase));
+          datum.push(Math.round(half + Math.log(-10 * v * inflation[year]) * pBase));
+        } else {
+          datum.push(Math.round(half - Math.log(10 * v) * pBase));
+          datum.push(Math.round(half - Math.log(10 * v * inflation[year]) * pBase));
+        }
+      }
+    }
+  }
+
+  setTimeout( function () {
+    calculatePoints();
+  }, 10);
+
+})();
+
