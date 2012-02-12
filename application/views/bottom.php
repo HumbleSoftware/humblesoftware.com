7e3f98acdf6d8e3a8d938d0e356b0c23befeb1a1
Carl Sutherland <carl@humblesoftware.com>
2012-02-12 10:01:35 -0500

    Moved analytics script.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -1,4 +1,17 @@
 </div> 
+<script type="text/javascript">
+  var HSD_BASE = '<?php echo base_url(); ?>';
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15129441-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <script src="https://www.google.com/jsapi?key=ABQIAAAAuThdpyh1Qwy4HBMceth4qxQwkqy4SzgAzJqTDqEXBNbWjToRhxR0PRivs2dDnyu_72HBYxlaIu-cOw"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <!--[if lt IE 9]>
--- application/views/top.php
+++ application/views/top.php
@@ -9,19 +9,6 @@
 <?php if (isset($includes)): foreach ($includes->getCSS() as $stylesheet) { ?>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url().'static/'.$stylesheet; ?>" />
 <?php } endif; ?>
-    <script type="text/javascript">
-      var HSD_BASE = '<?php echo base_url(); ?>';
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-15129441-1']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
 </head>
 
 <body>
