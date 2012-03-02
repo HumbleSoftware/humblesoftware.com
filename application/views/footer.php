cdf2e727987a7caed2e549d183ee8bd17e9f35b5
Carl Sutherland <carl@humblesoftware.com>
2012-03-02 15:01:03 -0500

    Initial new styles for footer, header.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,17 +1,21 @@
 <div id="footer-container">
     <div id="footer">
-        <span class="footer-text">
-            we let our work do the talking.
-        </span>
+        <span class="footer-text">we let our work do the talking.</span>
         <span class="footer-links">
-            
+            <ul>
+                <li>
+                    <a href="<?php echo base_url(); ?>home">home</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url(); ?>terms">terms</a>
+                </li>
+                <li>
+                    <a href="http://validator.w3.org/check?uri=referer">valid html5</a>
+                </li>
+            </ul>
         </span>
-        <span class="validation">
-            <a href="<?php echo base_url(); ?>home">home</a> | <a href="<?php echo base_url(); ?>terms">terms</a> | <a href="http://validator.w3.org/check?uri=referer">valid html5</a>
-        </span>
-         - 
         <span class="copyright">
-            &copy; <?php echo date('Y'); ?> <a href="<?php echo base_url(); ?>home">h.s.d.</a>
+            &copy; <?php echo date('Y'); ?> <a href="<?php echo base_url(); ?>home">hsd</a>
         </span>
     </div>
 </div>
