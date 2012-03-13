ab2c98327c464f768992a943c25cf8fc8d64b47c
Carl Sutherland <carl@humblesoftware.com>
2012-03-13 13:29:15 -0400

    Fix for mobile.

--- application/views/envision/index.php
+++ application/views/envision/index.php
@@ -4,6 +4,8 @@
 
   <h3><a href="<?php base_url() ?>demos/timeseries" title="HTML5 time series chart">Time Series:</a></h3>
 
+  <div class="image" id="timeseries-demo"></div>
+
   <p>
     HTML5 time series chart.  This uses the TimeSeries template.  Templates provide
     pre-built interactive visualizations matching common use-cases.  The TimeSeries 
@@ -13,13 +15,13 @@
     </span>
   </p>
 
-  <div class="image" id="timeseries-demo"></div>
-
 </div>
 
 <div class="demo">
 
   <h3><a href="<?php base_url() ?>demos/finance" title="HTML5 Finance Chart">Finance:</a></h3>
+
+  <div class="image" id="finance-demo"></div>
 
   <p>
     HTML5 financial chart.  This demos the financial charts with data in the global
@@ -30,13 +32,13 @@
     </span>
   </p>
 
-  <div class="image" id="finance-demo"></div>
-
 </div>
 
 <div class="demo">
 
   <h3><a href="<?php base_url() ?>demos/ajax" title="Dynamic HTML5 Finance Chart with Ajax">Ajax:</a></h3>
+
+  <div class="image" id="ajax-demo"></div>
 
   <p>
     AJAX driven financial chart.  This demo uses the same financial chart as the previous,
@@ -46,8 +48,6 @@
       (<a href="<?php base_url() ?>demos/ajax" title="Dynamic HTML5 Finance Chart with Ajax">click to edit</a>)
     </span>
   </p>
-
-  <div class="image" id="ajax-demo"></div>
 
 </div>
 
