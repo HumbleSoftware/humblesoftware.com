7cf11f16d16e228d443354fbc65ab5b8ed79bae2
Carl Sutherland <carl@humblesoftware.com>
2012-03-20 21:23:37 -0400

    Updates to demos index.

--- application/views/envision/index.php
+++ application/views/envision/index.php
@@ -1,17 +1,22 @@
+
 <div id="demos">
+
+<div class="stub">
+Envision.js is a library for creating fast, dynamic and interactive HTML5 visualizations.
+</div>
 
 <div class="demo">
 
-  <h3><a href="<?php echo base_url() ?>envision/demos/timeseries" title="HTML5 time series chart">Time Series:</a></h3>
+  <h3><a href="<?php echo base_url() ?>envision/demos/timeseries" title="HTML5 time series chart">TimeSeries:</a></h3>
 
   <div class="image" id="timeseries-demo"></div>
 
   <p>
     HTML5 time series chart.  This uses the TimeSeries template.  Templates provide
-    pre-built interactive visualizations matching common use-cases.  The TimeSeries 
+    pre-built interactive visualizations matching common use-cases.  TimeSeries 
     comes with zoom support.
     <span class="link">
-      (<a href="<?php echo base_url() ?>envision/demos/timeseries" title="HTML5 time series chart">click to edit</a>)
+      (<a href="<?php echo base_url() ?>envision/demos/timeseries" title="HTML5 time series chart">click to code</a>)
     </span>
   </p>
 
@@ -24,11 +29,11 @@
   <div class="image" id="finance-demo"></div>
 
   <p>
-    HTML5 financial chart.  This demos the financial charts with data in the global
+    HTML5 financial chart.  This demos the Finance template with data in the global
     namespace.  It specifies an initial selection and custom formatters for the x-axis
-    and mouse tracking.
+    labels and mouse tracking.
     <span class="link">
-      (<a href="<?php echo base_url() ?>envision/demos/finance" title="HTML5 Finance Chart">click to edit</a>)
+      (<a href="<?php echo base_url() ?>envision/demos/finance" title="HTML5 Finance Chart">click to code</a>)
     </span>
   </p>
 
@@ -41,11 +46,11 @@
   <div class="image" id="ajax-demo"></div>
 
   <p>
-    AJAX driven financial chart.  This demo uses the same financial chart as the previous,
-    but uses the selection interaction to be responsible for the data.  In this case the
-    selection range simply triggers an Ajax fetch, but this could be arbitrarily complex.
+    AJAX driven financial chart.  This demo uses the same Finance template as the previous,
+    but uses the selection interaction callback to manage data.  In this case the
+    selection range only triggers a single GET, but this could be arbitrarily complex.
     <span class="link">
-      (<a href="<?php echo base_url() ?>envision/demos/ajax" title="Dynamic HTML5 Finance Chart with Ajax">click to edit</a>)
+      (<a href="<?php echo base_url() ?>envision/demos/ajax" title="Dynamic HTML5 Finance Chart with Ajax">click to code</a>)
     </span>
   </p>
 
@@ -63,7 +68,7 @@
     Interaction for zooming.  The fractal itself is rendered using a custom
     flotr chart type.
     <span class="link">
-      (<a href="<?php echo base_url() ?>envision/demos/fractal" title="Custom Example - Fractal">click to edit</a>)
+      (<a href="<?php echo base_url() ?>envision/demos/fractal" title="Custom Example - Fractal">click to code</a>)
     </span>
   </p>
 
