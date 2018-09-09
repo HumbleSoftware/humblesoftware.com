815ccaa027d93fd67ceb5196cf6856cc482dd136
Carl Sutherland <carl@humblesoftware.com>
2018-09-09 11:04:08 -0400

    Add css bundling.
    Is this _really_ what we're doing in 2018?

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './resources/js/index.js',
+  entry: {
+    index: './resources/js/index.js',
+    styles: './resources/styles/index.js',
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'index.js'
+    filename: '[name].js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+      }
+    ]
   }
 };
