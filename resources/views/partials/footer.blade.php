caaa782af189090bf967c933e0ad7e2fe50f52ca
Carl Sutherland <carl@humblesoftware.com>
2019-01-11 19:06:09 -0500

    Footer & nav markup.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,11 @@
-<footer class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="/contact">developers of fine software</a>
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item"><span class="navbar-text">&copy; <?php echo date('Y'); ?> humble software development.</span></li>
-    <li class="nav-item"><a class="nav-link" href="/terms">terms</a></li>
-  </ul>
+<footer>
+  <div class="row">
+    <div class="col-sm-6">
+      <a href="/contact">developers of fine software</a>
+    </div>
+    <div class="col-sm-6 text-right">
+      <span>&copy; <?php echo date('Y'); ?> humble software development.</span>
+      <a href="/terms">terms</a>
+    </div>
+  </div>
 </footer>
