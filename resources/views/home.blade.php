500a314533f1fe1be8da78e3fb586b1e17047e7f
Carl Sutherland <carl@humblesoftware.com>
2019-01-12 16:31:59 -0500

    Home copy.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,11 @@
 @section('title', 'home')
 
 @section('content')
-    <p>This is my body content.</p>
+    <p>
+    Humble software development provides software design and development 
+    services and maintains several open source projects. Services include 
+    JavaScript/HTML5 application development, Canvas/SVG dynamic data
+    visualization, and search/information retrieval. Contact us or look
+    around to learn more.
+    </p>
 @endsection
