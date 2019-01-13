b9e0f91a470f570968c9784c8082466617450d33
Carl Sutherland <carl@humblesoftware.com>
2019-01-13 14:39:08 -0500

    Switch it up to scss.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,13 +3,14 @@ let mix = require('laravel-mix');
 mix
   .copyDirectory('resources/images', 'public/images')
   .js('resources/js/index.js', 'public')
+  .sass('resources/scss/index.scss', 'public')
   .combine([
     'resources/lib/envisionjs/envision.js',
     'resources/lib/flotr2/flotr2.examples.types.js',
   ], 'public/libs.js')
   .styles([
     'resources/lib/envisionjs/envision.css',
-    'resources/styles/style.css',
+    'public/index.css',
   ], 'public/styles.css')
   .sourceMaps()
   .browserSync({
