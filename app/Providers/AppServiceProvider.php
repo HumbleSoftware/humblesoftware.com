fdb62d93719322c54735c295dc9a692e18da4e9b
Carl Sutherland <carl@humblesoftware.com>
2019-01-19 14:31:43 -0500

    Add a listing directive.
    * wire up app service provider with facades
    * add blade listing directive.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      Blade::directive('listing', function ($filename) {
+        return file_get_contents("../resources/js/{$filename}");
+      });
+    }
+
     /**
      * Register any application services.
      *
@@ -13,6 +26,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 }
