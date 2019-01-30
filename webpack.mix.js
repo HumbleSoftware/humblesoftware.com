da350d16a11728cb530e4b72d0ef8a3b641ac986
Carl Sutherland <carl@humblesoftware.com>
2019-01-29 20:32:52 -0500

    Wire up editor libs.

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,11 @@ mix
   // JS:
   .js('resources/js/index.js', 'public')
   .combine([
+
+    // Envision & Flotr2:
     'resources/lib/envisionjs/envision.js',
+
+    // Flotr2 extra types, plugins & examples:
     'resources/lib/flotr2/js/types/bubbles.js',
     'resources/lib/flotr2/js/types/candles.js',
     'resources/lib/flotr2/js/types/gannt.js',
@@ -21,15 +25,35 @@ mix
     'resources/lib/flotr2/js/plugins/grid.js',
     'resources/lib/flotr2/js/plugins/spreadsheet.js',
     'resources/lib/flotr2/flotr2.examples.types.js',
+
+    // Editor:
+    'resources/lib/flotr2/examples/lib/codemirror/lib/codemirror.js',
+    'resources/lib/flotr2/examples/lib/codemirror/mode/javascript/javascript.js',
+    'resources/lib/flotr2/examples/lib/codemirror/mode/htmlmixed/htmlmixed.js',
+    'resources/lib/flotr2/examples/lib/codemirror/mode/css/css.js',
+    'resources/lib/flotr2/examples/lib/codemirror/mode/xml/xml.js',
+    'resources/lib/flotr2/examples/lib/beautify.js',
+    'resources/lib/flotr2/examples/lib/randomseed.js',
+    'resources/lib/flotr2/examples/js/Editor.js',
+
+    // D3:
     'resources/lib/d3.min.js',
+
   ], 'public/libs.js')
 
   // Styles:
   .sass('resources/scss/index.scss', 'public')
   .styles([
+
+    // Libs:
     'resources/lib/envisionjs/envision.css',
+    'resources/lib/flotr2/examples/lib/codemirror/lib/codemirror.css',
+    'resources/lib/flotr2/examples/editor.css',
+    'node_modules/code-prettify/src/prettify.css',
+
+    // Built SCSS:
     'public/index.css',
-    'node_modules/code-prettify/src/prettify.css'
+
   ], 'public/styles.css')
 
   // Dev:
